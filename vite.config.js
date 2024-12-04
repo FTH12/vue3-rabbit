3e26795a59ce6ac037bcb185c19dfdf179b5c4c2
rmh <r592413069@foxmail.com>
2024-12-04 22:04:50 +0800

    element主题色定制

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,36 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // element-plus 按需导入插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [
+        ElementPlusResolver({importStyle: "sass"}),
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData:
+        '@use "@/styles/element/index.scss" as *;',
+      }
+    }
+  }
 })
