45da88091ed60d734a9f61e9c71b7fc0cc7b993c
FTH12 <r592413069@foxmail.com>
2024-12-28 00:16:35 +0800

    登录-表单校验完成部分

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,31 @@
 <script setup>
+import { ref } from 'vue'
 
+const formData = ref({
+ account: '',
+ password: '',
+ agree: false
+})
+const rules = {
+  account: [
+    { required: true, message:'用户名不能为空', trigger:'blur' }
+  ],
+  password: [
+  { required: true, message:'密码不能为空', trigger:'blur' },
+  {min : 6,max :14, message:'密码应为6-14个字符', trigger: 'blur' }
+  ],
+  agree: [
+    { validator: (rule, value, callback)=>{
+      console.log(rule)
+
+      if(value){
+        callback()
+      }else{
+        callback(new Error('请同意用户协议！'))
+      }
+    }, trigger: 'blur' }
+  ]
+}
 </script>
 
 
@@ -25,15 +51,17 @@
         <div class="account-box">
           <div class="form">
             <el-form label-position="right" label-width="60px"
-              status-icon>
-              <el-form-item  label="账户">
-                <el-input/>
+              status-icon
+              :rules="rules"
+              :model="formData">
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="formData.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input/>
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="formData.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox v-model="formData.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
