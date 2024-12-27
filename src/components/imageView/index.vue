dec66b5da4ea24d4a7dde1d47a967ab140bb93e8
rmh <r592413069@foxmail.com>
2024-12-27 16:23:33 +0800

    图片预览组件完成

--- src/components/imageView/index.vue
+++ src/components/imageView/index.vue
@@ -3,14 +3,14 @@ import { useMouseInElement } from '@vueuse/core'
 import { ref } from 'vue'
 import { watch } from 'vue'
 
-// 图片列表
-const imageList = [
-  "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
-  "https://yanxuan-item.nosdn.127.net/e801b9572f0b0c02a52952b01adab967.jpg",
-  "https://yanxuan-item.nosdn.127.net/b52c447ad472d51adbdde1a83f550ac2.jpg",
-  "https://yanxuan-item.nosdn.127.net/f93243224dc37674dfca5874fe089c60.jpg",
-  "https://yanxuan-item.nosdn.127.net/f881cfe7de9a576aaeea6ee0d1d24823.jpg"
-]
+const props = defineProps({
+  imageList:{
+    type: Array,
+    default: ()=>[]
+  }
+})
+
+
 // 小图切换大图显示
 const acitveIndex = ref(0)
 const enterHandle = (i)=>{
@@ -22,6 +22,8 @@ const { elementX, elementY, isOutside} = useMouseInElement(target)
 const left = ref(0)
 const top = ref(0)
 watch([elementX, elementY], ()=>{
+  // 如果鼠标不在盒子内，则不执行后面的逻辑
+  if(isOutside.value) return
   if (elementX.value >= 100 && elementX.value <= 300){
     left.value = elementX.value-100
   }
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -34,7 +34,7 @@ onMounted(() => getGoods())
           <div class="goods-info">
             <div class="media">
               <!-- 图片预览区 -->
-              <image-view></image-view>
+              <image-view :image-list="goods.mainPictures"></image-view>
               <!-- 统计数量 -->
               <ul class="goods-sales">
                 <li>
