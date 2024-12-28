b1f797fa2eee788ea409907c1bcbd57fccaf2bda
FTH12 <r592413069@foxmail.com>
2024-12-29 02:05:47 +0800

    购物车列表统计计算

--- src/stores/cart.js
+++ src/stores/cart.js
@@ -17,6 +17,10 @@ export const useCartStore = defineStore('cart',()=>{
     cartList.value = cartList.value.filter( (item)=> item.skuId !== skuId)
   }
 
+  const changeSelected = (skuId, selected) => {
+    const item = cartList.value.find((item)=>item.skuId === skuId)
+    item.selected = selected
+  }
   // 计算属性
   const allCount = computed(()=>
     cartList.value.reduce((sumCount, item)=> sumCount+item.count, 0)
@@ -26,12 +30,36 @@ export const useCartStore = defineStore('cart',()=>{
     cartList.value.reduce((sumPrice, item)=> sumPrice+item.price*item.count,0)
   )
 
+  const selectedCount = computed(()=>
+    cartList.value.reduce((sumCount, item)=> {
+      if(item.selected){
+        sumCount+=1
+      }
+      return sumCount
+    },0)
+  )
+
+  const selectAll = computed(()=>
+    cartList.value.reduce((now, item)=> now && item.selected, true)
+  )
+  const changeAll = (allChange)=>{
+    cartList.value.forEach((item)=> item.selected=allChange)
+  }
+
+  const selectPrice = computed(()=>
+    cartList.value.reduce((sumPrice, item)=> item.selected ? sumPrice+item.price*item.count:sumPrice, 0)
+  )
   return {
     cartList,
     addCart,
     delCart,
+    changeSelected,
     allCount,
     allPrice,
+    selectedCount,
+    selectAll,
+    changeAll,
+    selectPrice
   }
 },{
   persist:{
